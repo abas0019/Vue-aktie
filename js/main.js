117b7d917f7a0626ddc62d3176584345de1b42d9
Abasin Mohammed <147157387+abas0019@users.noreply.github.com>
2024-10-29 14:17:52 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            intro: 'Welcome to my Vue template',
+            
+        }
+    },
+    methods: {
+        myMethod(){
+
+        },
+    },
+    computed: {
+        myComputed() {
+            return ''
+        },
+        
+    }
+})
